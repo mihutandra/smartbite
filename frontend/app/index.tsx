ec1a16ee2d29f934edb4f546d876e6f3b020a4f3
Mara <valeanmara@gmail.com>
2026-03-29 20:03:24 +0300

    add login screen without backend connections

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,5 @@
-import { Text, View } from "react-native";
+import { LoginScreen } from "../components/auth/LoginScreen";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <LoginScreen />;
 }
