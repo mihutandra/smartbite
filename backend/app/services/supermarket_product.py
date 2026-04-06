34ba9d2d97d45cd3779c1a634241823e0482a4ba
mihutandra <andra.mihut@stud.ubbcluj.ro>
2026-04-06 21:54:39 +0300

    service and router

---
+++ backend/app/services/supermarket_product.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from app.exceptions.exceptions import NotFound
+from app.models.supermarket_products import SupermarketProduct
+from app.repositories.supermarket_product import SupermarketProductRepository
+
+
+class SupermarketProductService:
+    def __init__(self, supermarket_product_repo: SupermarketProductRepository):
+        self.supermarket_product_repo = supermarket_product_repo
+
+    def list_all(self, page: int, page_size: int) -> list[SupermarketProduct]:
+        offset = (page - 1) * page_size
+        return self.supermarket_product_repo.get_all(limit=page_size, offset=offset)
+
+    def get_by_id(self, supermarket_product_id: UUID) -> SupermarketProduct:
+        supermarket_product = self.supermarket_product_repo.get_by_id(supermarket_product_id)
+        if not supermarket_product:
+            raise NotFound(entity="SupermarketProduct", identifier=str(supermarket_product_id))
+        return supermarket_product
