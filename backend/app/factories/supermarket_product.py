f26b6e11706935d3068699a7496b6e85ec34dadb
Ana Tatar <94306011+anatatar10@users.noreply.github.com>
2026-04-24 22:41:40 +0300

    delete get_supermarket_repo from factories/supermarket_product

--- backend/app/factories/supermarket_product.py
+++ backend/app/factories/supermarket_product.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Session
 from app.core.database import get_session
 from app.repositories.supermarket_product import SupermarketProductRepository
 from app.services.supermarket_product import SupermarketProductService
-from app.factories.supermarket import get_supermarket_repository
 from app.repositories.supermarket import SupermarketRepository
+from app.factories.supermarket import get_supermarket_repository
 
 
 def get_supermarket_product_repository(
@@ -12,10 +12,15 @@ def get_supermarket_product_repository(
 ) -> SupermarketProductRepository:
     return SupermarketProductRepository(session=db_session)
 
+def get_supermarket_repository(
+        db_session: Session = Depends(get_session),
+) -> SupermarketRepository:
+    return SupermarketRepository(session=db_session)
+
 
 def get_supermarket_product_service(
-    supermarket_product_repo: SupermarketProductRepository = Depends(get_supermarket_product_repository),
-    supermarket_repo: SupermarketRepository = Depends(get_supermarket_repository),
+        supermarket_product_repo: SupermarketProductRepository = Depends(get_supermarket_product_repository),
+        supermarket_repo: SupermarketRepository = Depends(get_supermarket_repository),
 ) -> SupermarketProductService:
     return SupermarketProductService(
         supermarket_product_repo=supermarket_product_repo,
