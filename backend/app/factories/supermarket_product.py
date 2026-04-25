407de13fd08b464a2d496fd2e28b798a1cbe93eb
Ana Tatar <94306011+anatatar10@users.noreply.github.com>
2026-04-25 11:13:56 +0300

    delete get_supermarket_repo from factories/supermarket_product

--- backend/app/factories/supermarket_product.py
+++ backend/app/factories/supermarket_product.py
@@ -12,12 +12,6 @@ def get_supermarket_product_repository(
 ) -> SupermarketProductRepository:
     return SupermarketProductRepository(session=db_session)
 
-def get_supermarket_repository(
-        db_session: Session = Depends(get_session),
-) -> SupermarketRepository:
-    return SupermarketRepository(session=db_session)
-
-
 def get_supermarket_product_service(
         supermarket_product_repo: SupermarketProductRepository = Depends(get_supermarket_product_repository),
         supermarket_repo: SupermarketRepository = Depends(get_supermarket_repository),
