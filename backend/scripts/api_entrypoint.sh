60bc010067ccf5ba8283812c15592fbc36d56644
mihutandra <andra.mihut@stud.ubbcluj.ro>
2026-05-24 15:23:12 +0300

    feat(new products): omfg i made it

--- backend/scripts/api_entrypoint.sh
+++ backend/scripts/api_entrypoint.sh
@@ -16,4 +16,11 @@ python scripts/seed_supermarket_products.py
 echo "Seeding users..."
 python scripts/seed_users.py
 
+# new seeds !!!!!!!
+echo "Seeding categories from json"
+python scripts/seed_categories_from_json.py
+
+echo "Seeding products from json"
+python scripts/seed_products_from_json.py
+
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 "$@"
